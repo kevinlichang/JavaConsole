3e4e3886bea9d9599566feea6d236034fc8b4cad
Kchar <38297595+kevinlichang@users.noreply.github.com>
2019-01-31 18:20:02 -0500

    java project

---
+++ Spark/src/yourworkhere/ExampleAccount.java
@@ -0,0 +1,66 @@
+package yourworkhere;
+
+public class ExampleAccount {
+	
+	
+	public static void main(String[] args) {
+		//TODO 	1.2.2 Define the data types below (balance, withdrawalAmount, depositAmount, accountName, accountType)
+		double balance = 2000.00;
+		
+		double withdrawalAmount = 500.25;
+		
+		double depositAmount = 645.18;
+		
+		String accountName = "KC Account";
+		
+		String accountType = "Savings";
+				
+		
+		//TODO	1.2.3 Print the value of balance below
+		System.out.println(balance);
+		
+		System.out.println(withdrawalAmount);
+		
+		System.out.println(depositAmount);
+		
+		System.out.println(accountName);
+		
+		System.out.println(accountType);
+		
+		//TODO 	1.2.4 Write a short bit of code to execute a withdrawal of 500.25 from the value of balance
+		//		Print the result
+		double currentBalance = balance - withdrawalAmount;
+		System.out.println(currentBalance);
+		
+		//TODO	1.2.5 Write a short snippet of code to deposit 645.18 to the current value of balance
+		//		Print the result
+		currentBalance = currentBalance + depositAmount;
+		System.out.println(currentBalance);		
+		
+		/*
+		 * TODO 1.2.6 Write the lines to check the balance if you added compound interest for a year
+		 * 
+		 * Step 1: Define the variables Use A, P, r, n, t You must decide what datatypes
+		 * to make each according to their purpose: 
+		 * A - Accrued Amount; this is the amount you're solving for 
+		 * P - Principle; set this to the current balance. 
+		 * r - rate of interest; set this to 0.15 
+		 * n - number of times the amount is compounded; set this to 12 
+		 * t - number of years; set this to 1 to represent 1 year
+		 * 
+		 * Step 2: Calculate the Accrued Amount Use the formula: A = P(1 + r/n)nt
+		 * 
+		 * Step 3: Print the result to the console
+		 */
+		
+		double A;
+		double P = currentBalance;
+		double r = 0.15;
+		int n = 12;
+		int t = 1;
+		
+		A = P * (1 + r/n) * n * t;
+		
+		System.out.println(A);
+	}
+}
---
+++ Spark/src/yourworkhere/ExampleAccount2.java
@@ -0,0 +1,23 @@
+package yourworkhere;
+
+public class ExampleAccount2 {
+
+	public static void main(String[] args) {
+		//TODO 	1.3.1 Define the data types below (balance, withdrawalAmount, depositAmount)
+		
+		//TODO	1.3.2 Initialize values for the above variables
+		//		balance should be 10000.00
+		//		withdrawalAmount should be 50002.21
+		//		depositAmount should be -40020.02
+		
+		//TODO	1.3.3 Write a conditional statement to check if withdrawalAmount is larger than balance
+		//		If it is then, print "Withdrawal will result in an overdraft!"
+		
+		
+		//TODO 	1.3.4 Write a conditional statement to check if depositAmount is less than or equal to 0
+		//		If so, then print "Deposit amount cannot be negative!"
+		//		Otherwise, then add the deposit amount to the balance and print the new balance value
+		
+		
+	}
+}
